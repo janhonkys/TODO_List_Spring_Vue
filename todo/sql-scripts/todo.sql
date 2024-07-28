d512de667a171d808b45378f043cb3eef567ff60
janhonkys <honkys.j1@seznam.cz>
2024-07-28 10:35:40 +0200

    Updated sql script

--- todo/sql-scripts/todo.sql
+++ todo/sql-scripts/todo.sql
@@ -13,7 +13,7 @@ CREATE Table todo (
 INSERT INTO todo(`id`, `title`, `description`, `priority`, `date`) VALUES(1, 'Cook', 'Today I have to cook dinner.', 'Low', '2024-09-24'),
 					(2, 'TV', 'Today I have to watch TV.', 'Medium', '2024-09-23'),
                     (3, 'Bike', 'Today I have to ride a bike.', 'High', '2024-09-25'),
-                    (4, 'Clean', 'Today I have to clean bathroom.', 'High', '2024-09-22');
+                    (4, 'Clean', 'Today I have to clean bathroom..', 'High', '2024-09-22');
 TRUNCATE todo;
 SELECT * FROM todo;
 
